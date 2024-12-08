f836589fa14841d605298a6046424ab5b25613d8
Natalya Botsman <botsmannatashaa@gmail.com>
2024-12-09 01:18:15 +0600

    Created Toolbar component

---
+++ frontend/src/components/UI/AppToolbar/AppToolbar.tsx
@@ -0,0 +1,25 @@
+import { AppBar, styled, Toolbar, Typography } from '@mui/material';
+import { Link as NavLink } from 'react-router-dom';
+
+const Link = styled(NavLink)({
+  color: 'inherit',
+  textDecoration: 'none',
+  '&:hover': {
+    color: 'inherit'
+  },
+});
+
+const AppToolbar = () => {
+  return (
+    <AppBar position="sticky" sx={{mb: 2}}>
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
+          <Link to="/">Chat</Link>
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default AppToolbar;
+
