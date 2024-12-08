596a35342d7c461a8f67f7c070e64dd0dbe73be6
Natalya Botsman <botsmannatashaa@gmail.com>
2024-12-08 19:09:24 +0600

    Created index.ts to control creating of db.json file & call of init() method from fileDb.ts

--- api/index.ts
+++ api/index.ts
@@ -1,15 +1,23 @@
 import express from 'express';
 import cors from 'cors';
 import messagesRouter from './routers/messages';
+import fileDb from "./fileDb";
+import fs = require("fs");
 
 const app = express();
 const port = 8000;
 
 app.use(cors());
 app.use(express.json());
-app.use('/', messagesRouter);
+app.use('/messages', messagesRouter);
 
 const run: () => Promise<void> = async () => {
+    if (!fs.existsSync('./db.json')) {
+        fs.writeFileSync('./db.json', JSON.stringify([]));
+    }
+
+    await fileDb.init();
+
     app.listen(port, () => {
         console.log(`Listening on port http://localhost:${port}`);
     });
