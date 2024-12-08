e6e47b856515ce2e8a2d1b2d05b663dd5440d707
Natalya Botsman <botsmannatashaa@gmail.com>
2024-12-08 19:02:52 +0600

    1. Created router file, added 2 requests: post & get. 2. Get request consists of 2 validations: if we get datetime in url - getMessagesByDate() function is called and returns list of messages by this date exclude this date, otherwise getMessages() is called

--- api/routers/messages.ts
+++ api/routers/messages.ts
@@ -1,5 +1,45 @@
-import express from "express";
+import express from 'express';
+import {IMessage, IMessageMutation} from '../types';
+import fileDb from '../fileDb';
 
 const messagesRouter = express.Router();
+
+messagesRouter.get('/', async (req, res) => {
+    try {
+        const queryDate = req.query.datetime as string;
+
+        if (queryDate) {
+            const date = new Date(queryDate);
+
+            if (isNaN(date.getDate())) {
+                res.status(400).send({error: 'Invalid date'});
+                return;
+            }
+
+            const messages: IMessage[] = await fileDb.getMessagesByDate(date);
+            res.send(messages.length ? messages.slice(0, 30) : []);
+        } else {
+            const messages: IMessage[] = await fileDb.getMessages();
+            res.send(messages.length ? messages.slice(0, 30) : []);
+        }
+    } catch (e) {
+        console.error(e);
+    }
+});
+
+messagesRouter.post('/', async (req, res) => {
+    const message: IMessageMutation = {
+        author: req.body.author,
+        message: req.body.message
+    };
+
+    if (!message.author || !message.message || !message.author.trim().length || !message.message.trim().length) {
+        res.status(400).send({error: 'Author and message are required and cannot be empty'});
+        return;
+    }
+
+    const newMessage = await fileDb.addMessage(message);
+    res.send(newMessage);
+});
 
 export default messagesRouter;
