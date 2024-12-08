13e5185b9e58ee5ad687c0a9746cbdfad262dc06
Natalya Botsman <botsmannatashaa@gmail.com>
2024-12-09 01:41:11 +0600

    Created 3 async thunks: 1. Fetches 30 messages from the db 2. Fetches sorted list of messages by datetime parameter passed to url 3. Adds a new message

---
+++ frontend/src/store/thunks/messagesThunk.ts
@@ -0,0 +1,26 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { IMessage, IMessageMutation } from '../../types';
+import axiosApi from '../../axiosApi.ts';
+
+export const fetchMessages = createAsyncThunk<IMessage[], void>
+('messages/fetchMessages',
+  async () => {
+    const response = await axiosApi.get<IMessage[]>('/messages');
+    return response.data;
+  }
+);
+
+export const getMessagesByDate = createAsyncThunk<IMessage[], string>(
+  'messages/getMessagesByDate',
+  async (datetime: string) => {
+    const response = await axiosApi.get<IMessage[]>(`/messages?datetime=${datetime}`);
+    return response.data;
+  }
+);
+
+export const createMessage = createAsyncThunk<void, IMessageMutation>(
+  'messages/createMessage',
+  async (message: IMessage) => {
+    await axiosApi.post('/messages', {...message});
+  }
+);
