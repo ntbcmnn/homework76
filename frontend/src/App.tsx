69ef2a8cbb33bd78001efc44ca7523e5a0c3b9ff
Natalya Botsman <botsmannatashaa@gmail.com>
2024-12-09 01:32:32 +0600

    1. Displayed Toolbar 2. Displayed Form component 3. Added Routes &, using them, displayed Messages component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,26 @@
 import './App.css';
+import Messages from './containers/Messages/Messages';
+import AppToolbar from './components/UI/AppToolbar/AppToolbar.tsx';
+import { Container, CssBaseline } from '@mui/material';
+import Form from './containers/Form/Form.tsx';
+import { Route, Routes } from 'react-router-dom';
 
-const App = () => <></>;
+const App = () => <>
+  <CssBaseline/>
+
+  <header>
+    <AppToolbar/>
+  </header>
+
+  <main>
+    <Container maxWidth="xl">
+      <Form/>
+      <Routes>
+        <Route path="/" element={<Messages/>}/>
+        <Route path="/messages" element={<Messages/>}/>
+      </Routes>
+    </Container>
+  </main>
+</>;
 
 export default App;
