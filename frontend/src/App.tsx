c197de5084d3b160d0f33efaa2f2a77aa8497428
Natalya Botsman <botsmannatashaa@gmail.com>
2024-12-09 16:57:14 +0600

    Removed routers, because they're not being used

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,6 @@ import './App.css';
 import Messages from './containers/Messages/Messages';
 import AppToolbar from './components/UI/AppToolbar/AppToolbar.tsx';
 import { Container, CssBaseline } from '@mui/material';
-import Form from './containers/Form/Form.tsx';
-import { Route, Routes } from 'react-router-dom';
 
 const App = () => <>
   <CssBaseline/>
@@ -14,11 +12,7 @@ const App = () => <>
 
   <main>
     <Container maxWidth="xl">
-      <Form/>
-      <Routes>
-        <Route path="/" element={<Messages/>}/>
-        <Route path="/messages" element={<Messages/>}/>
-      </Routes>
+      <Messages/>
     </Container>
   </main>
 </>;
